70947b351e12c56199f59fb3dd9d17ff83940e82
brianschwartz <brian.schwartz@mymail.nwtc.edu>
2015-10-18 15:43:17 -0500

    Added: 	code added that ruth emailed about under question button clicks 	listed what is still needed with comments in click events

--- src/trivia/gui/QuestionSelectFrame.java
+++ src/trivia/gui/QuestionSelectFrame.java
@@ -13,6 +13,8 @@ import trivia.Game;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 
@@ -81,7 +83,7 @@ public class QuestionSelectFrame extends JFrame {
 	public QuestionSelectFrame(Game game) throws IOException {
 		this.game = game;
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 555, 331);
+		setBounds(100, 100, 562, 354);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -94,14 +96,61 @@ public class QuestionSelectFrame extends JFrame {
 		
 		
 		JButton btnQuestion1 = new JButton(questions[0]);
+		btnQuestion1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				/* the following code will eventually need to pass the question being chosen
+				 * the leader will also have to be taken into consideration for whos turn it is
+				 * to answer the question
+				*/
+				//added for ruth
+				//--------------------------------------------------------------------------//
+				game.setCurrentPlayer(0); //just before
+				new AnswerEntryFrame(game).setVisible(true);
+				QuestionSelectFrame.this.dispose();
+				//--------------------------------------------------------------------------//
+			}
+		});
+		
+		
 		btnQuestion1.setBounds(86, 92, 369, 31);
 		contentPane.add(btnQuestion1);
 		
 		JButton btnQuestion2 = new JButton(questions[1]);
+		btnQuestion2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				/* the following code will eventually need to pass the question being chosen
+				 * the leader will also have to be taken into consideration for whos turn it is
+				 * to answer the question
+				*/
+				//added for ruth
+				//--------------------------------------------------------------------------//
+				game.setCurrentPlayer(0); //just before
+				new AnswerEntryFrame(game).setVisible(true);
+				QuestionSelectFrame.this.dispose();
+				//--------------------------------------------------------------------------//
+			}
+		});
 		btnQuestion2.setBounds(86, 142, 369, 31);
 		contentPane.add(btnQuestion2);
 		
 		JButton btnQuestion3 = new JButton(questions[2]);
+		btnQuestion3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				/* the following code will eventually need to pass the question being chosen
+				 * the leader will also have to be taken into consideration for whos turn it is
+				 * to answer the question
+				*/
+				//added for ruth
+				//--------------------------------------------------------------------------//
+				game.setCurrentPlayer(0); //just before
+				new AnswerEntryFrame(game).setVisible(true);
+				QuestionSelectFrame.this.dispose();
+				//--------------------------------------------------------------------------//
+			}
+		});
 		btnQuestion3.setBounds(86, 192, 369, 31);
 		contentPane.add(btnQuestion3);
 		
@@ -110,6 +159,4 @@ public class QuestionSelectFrame extends JFrame {
 		lblPlayerPleaseSelect.setBounds(17, 24, 344, 49);
 		contentPane.add(lblPlayerPleaseSelect);
 	}
-	
-	
 }
