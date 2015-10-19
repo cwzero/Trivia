821832a3030c929cdf5b8d9bc7d7313e50973aee
Laercio <laercio.catao@gmail.com>
2015-10-18 22:25:40 -0500

    Just Started the ScoreBoard as a Table type
    I will need to know where we will save the file to read values

--- src/trivia/gui/GameStatusFrame.java
+++ src/trivia/gui/GameStatusFrame.java
@@ -3,14 +3,20 @@ package trivia.gui;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
+import javax.swing.JScrollPane;
 import trivia.Game;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.Font;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
 
 @SuppressWarnings("serial")
 public class GameStatusFrame extends JFrame {
 
 	private JPanel contentPane;
 	private Game game;
+	private JTable table;
 
 	/**
 	 * Create the frame.
@@ -24,6 +30,19 @@ public class GameStatusFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblScoreboard = DefaultComponentFactory.getInstance().createLabel("SCOREBOARD");
+		lblScoreboard.setFont(new Font("Lithos Pro Regular", Font.ITALIC, 16));
+		lblScoreboard.setBounds(153, 11, 127, 28);
+		contentPane.add(lblScoreboard);
+		
+		Object rowData[][] = { { "Row1-Column1", "Row1-Column2", "Row1-Column3" },
+								{ "Row2-Column1", "Row2-Column2", "Row2-Column3" } };
+		Object columnNames[] = { "Player", "Total Questions", "Scores" };
+		JTable table = new JTable(rowData, columnNames);
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setLocation(10, 50);
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		scrollPane.setSize(414, 189);
 	}
-
 }
