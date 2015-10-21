f96fd5698989e205fc8f93b07a5f90cb219a424e
Alesander Romero <alesander.romero@mymail.nwtc.edu>
2015-10-21 09:38:51 -0500

    Not finished yet

--- src/trivia/gui/WinnerSelectFrame.java
+++ src/trivia/gui/WinnerSelectFrame.java
@@ -5,6 +5,13 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import trivia.Game;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+import java.awt.Component;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class WinnerSelectFrame extends JFrame {
@@ -18,11 +25,37 @@ public class WinnerSelectFrame extends JFrame {
 	public WinnerSelectFrame(Game game) {
 		this.game = game;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 282, 296);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-	}
+		
+		JLabel lblNewLabel = new JLabel("Select one answer for the question:");
+		lblNewLabel.setBounds(36, 11, 178, 38);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnSelectWinner = new JButton("Select Winner");
+		btnSelectWinner.setBounds(24, 218, 203, 28);
+		contentPane.add(btnSelectWinner);
+		
+		JLabel lblSelectedQuestion = new JLabel("New label");
+		lblSelectedQuestion.setBounds(24, 51, 203, 28);
+		contentPane.add(lblSelectedQuestion);
+		lblSelectedQuestion.setText("Question");
+		
 
+		DefaultListModel<String> listModel = new DefaultListModel<>();
+		for (int i=0; i<game.getPlayerCount(); i++)
+		{
+		listModel.addElement(game.setPlayerAnswer(i, playerAnswer));
+		}
+        
+        JList lstAnswers = new JList<>(listModel);
+		lstAnswers.setBounds(24, 92, 203, 115);
+		contentPane.add(lstAnswers);
+        
+        add(lstAnswers);
+		
+	}
 }
