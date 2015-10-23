d770f4460375609e480f5fdfd28b0bd45bd4c3a4
Eric Gjerde <gjerdeeric@gmail.com>
2015-10-23 18:09:23 -0500

    Break logic for buttons into method

--- src/trivia/gui/QuestionSelectFrame.java
+++ src/trivia/gui/QuestionSelectFrame.java
@@ -1,6 +1,5 @@
 package trivia.gui;
 import java.io.IOException;
-import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,7 +17,11 @@ public class QuestionSelectFrame extends JFrame {
 
 	private JPanel contentPane;
 	private Game game;
-	
+	private String[] questions = new String[3];
+	private JButton btnQuestion1;
+	private JButton btnQuestion2;
+	private JButton btnQuestion3;
+
 	/**
 	 * Create the frame.
 	 * 
@@ -35,90 +38,56 @@ public class QuestionSelectFrame extends JFrame {
 
 		// called the new method openfile
 		// returns an array into questions
-		String[] questions = game.getQuestionPool();
-		
+		questions = game.getQuestionPool(3);
+
 		// switched button labels to items in the questions array
 
 		// Rather than using separate buttons for each question, maybe a label
 		// and radio button, with one button to move on
 
-		JButton btnQuestion1 = new JButton(questions[0]);
+		btnQuestion1 = new JButton(questions[0]);
 		btnQuestion1.addActionListener(new ActionListener() {
-			private String currentQuestion = btnQuestion1.getText();
-
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-
-				/*
-				 * the following code will eventually need to pass the question
-				 * being chosen the leader will also have to be taken into
-				 * consideration for whos turn it is to answer the question
-				 */
-				// added for ruth
-				// --------------------------------------------------------------------------//
-				game.setCurrentPlayer(0); // just before
-				game.setCurrentQuestion(currentQuestion);
-				QuestionSelectFrame.this.dispose();
-				new AnswerEntryFrame(game).setVisible(true);
-				// --------------------------------------------------------------------------//
+				QuestionSelectFrame.this.selectQuestion(0);
 			}
 		});
 
 		btnQuestion1.setBounds(86, 92, 369, 31);
 		contentPane.add(btnQuestion1);
 
-		JButton btnQuestion2 = new JButton(questions[1]);
+		btnQuestion2 = new JButton(questions[1]);
 		btnQuestion2.addActionListener(new ActionListener() {
-			private String currentQuestion = btnQuestion2.getText();
-
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-
-				/*
-				 * the following code will eventually need to pass the question
-				 * being chosen the leader will also have to be taken into
-				 * consideration for whos turn it is to answer the question
-				 */
-				// added for ruth
-				// --------------------------------------------------------------------------//
-				game.setCurrentPlayer(0); // just before
-				game.setCurrentQuestion(currentQuestion);
-				QuestionSelectFrame.this.dispose();
-				new AnswerEntryFrame(game).setVisible(true);
-				// --------------------------------------------------------------------------//
+				QuestionSelectFrame.this.selectQuestion(1);
 			}
 		});
 		btnQuestion2.setBounds(86, 142, 369, 31);
 		contentPane.add(btnQuestion2);
 
-		JButton btnQuestion3 = new JButton(questions[2]);
+		btnQuestion3 = new JButton(questions[2]);
 		btnQuestion3.addActionListener(new ActionListener() {
-			private String currentQuestion = btnQuestion3.getText();
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
-				/*
-				 * the following code will eventually need to pass the question
-				 * being chosen the leader will also have to be taken into
-				 * consideration for whos turn it is to answer the question
-				 */
-				// added for ruth
-				// --------------------------------------------------------------------------//
-				game.setCurrentPlayer(0); // just before
-				game.setCurrentQuestion(currentQuestion);
-				QuestionSelectFrame.this.dispose();
-				new AnswerEntryFrame(game).setVisible(true);
-				// --------------------------------------------------------------------------//
+				QuestionSelectFrame.this.selectQuestion(2);
 			}
 		});
 		btnQuestion3.setBounds(86, 192, 369, 31);
 		contentPane.add(btnQuestion3);
 
 		JLabel lblPlayerPleaseSelect = new JLabel(
-				game.getPlayerNames()[game.getCurrentLeader()] + ", please select question:");
+				game.getPlayerNames()[game.getCurrentLeader()]
+						+ ", please select question:");
 		lblPlayerPleaseSelect.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblPlayerPleaseSelect.setBounds(17, 24, 344, 49);
 		contentPane.add(lblPlayerPleaseSelect);
+	}
+
+	public void selectQuestion(int question) {
+		game.setCurrentPlayer(0);
+		game.setCurrentQuestion(questions[question]);
+		this.dispose();
+		new AnswerEntryFrame(game).setVisible(true);
 	}
 }
