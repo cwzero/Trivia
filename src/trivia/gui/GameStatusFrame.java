01434d579ed318dd2600a4bc4bb5aa8e9eab2045
22117158 <22117158@STU_BI208-07.NWTC.EDU>
2015-10-23 17:27:11 -0500

    Added a button for start new game Change the labels and some comments

--- src/trivia/gui/GameStatusFrame.java
+++ src/trivia/gui/GameStatusFrame.java
@@ -9,6 +9,7 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTable;
 import java.awt.BorderLayout;
+import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class GameStatusFrame extends JFrame {
@@ -29,19 +30,30 @@ public class GameStatusFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
+		
+		// This create the label for the Scoreboard
+		
 		JLabel lblScoreboard = new JLabel("Scoreboard");
 		lblScoreboard.setFont(new Font("Lithos Pro Regular", Font.ITALIC, 16));
 		lblScoreboard.setBounds(153, 11, 127, 28);
 		contentPane.add(lblScoreboard);
-
+		
+		// This is just to hold rows and columns for now but will need to read from
+		// a file or a list before displaying the player and scores
+		
 		Object rowData[][] = {{"Row1-Column1", "Row1-Column2", "Row1-Column3"},
 				{"Row2-Column1", "Row2-Column2", "Row2-Column3"}};
-		Object columnNames[] = {"Player", "Total Questions", "Scores"};
+		Object columnNames[] = {"Players", "Scores", "Right Answers"};
 		JTable table = new JTable(rowData, columnNames);
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setLocation(10, 50);
 		contentPane.add(scrollPane, BorderLayout.CENTER);
-		scrollPane.setSize(414, 189);
+		scrollPane.setSize(414, 150);
+		
+		// This button starts new game " Not valid yet" 
+		
+		JButton btnStartNewGame = new JButton("Start New Game");
+		btnStartNewGame.setBounds(153, 228, 113, 23);
+		contentPane.add(btnStartNewGame);
 	}
 }
