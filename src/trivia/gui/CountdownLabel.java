ebcfbceecb47d514a523c1c6d21efc25bc49dc38
Eric Gjerde <gjerdeeric@gmail.com>
2015-11-06 18:18:57 -0600

    Add CountdownLabel class

---
+++ src/trivia/gui/CountdownLabel.java
@@ -0,0 +1,24 @@
+package trivia.gui;
+
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class CountdownLabel extends JLabel {
+	public CountdownLabel(int length) {
+		this.setText(length + "");
+		Thread countdownThread = new Thread() {
+			public void run() {
+				for (int a = length; a > 0; a++) {
+					CountdownLabel.this.setText(a + "");
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+		countdownThread.start();
+	}
+}
