f2b7cc632ad6e8467d4e26108ddc659722327a8f
Eric Gjerde <gjerdeeric@gmail.com>
2015-11-06 18:43:25 -0600

    Fix countdownlabel

--- src/trivia/gui/CountdownLabel.java
+++ src/trivia/gui/CountdownLabel.java
@@ -3,22 +3,24 @@ package trivia.gui;
 import javax.swing.JLabel;
 
 @SuppressWarnings("serial")
-public class CountdownLabel extends JLabel {
+public abstract class CountdownLabel extends JLabel {
 	public CountdownLabel(int length) {
 		this.setText(length + "");
 		Thread countdownThread = new Thread() {
 			public void run() {
-				for (int a = length; a > 0; a++) {
+				for (int a = length; a >= 0; a--) {
 					CountdownLabel.this.setText(a + "");
-				}
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 		};
 		countdownThread.start();
 	}
+	
+	public void whenComplete() {}
 }
