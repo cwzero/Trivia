a0b2b3ee2cdaead188aed1ff7e580f47a473649a
Eric Gjerde <gjerdeeric@gmail.com>
2015-11-13 17:59:07 -0600

    Update

--- src/trivia/db/TriviaDatabase.java
+++ src/trivia/db/TriviaDatabase.java
@@ -13,45 +13,43 @@ import trivia.Question;
 public class TriviaDatabase {
 	private static boolean initialized = false;
 	private static Connection connection = null;
-	
+
 	private static String host = "trivia.c8jug9wmu3by.us-east-1.rds.amazonaws.com";
 	private static int port = 3306;
 	private static String db = "Trivia";
-	
+
 	private static String user = "trivia";
 	private static String pass = "trivia";
-	
+
 	private static List<Question> questions;
 
 	public static void init() {
 		if (!initialized) {
 			try {
-				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				Class.forName("com.mysql.jdbc.Driver");
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
 			}
-			
+
 			Statement statement = null;
 			ResultSet resultSet = null;
 			questions = new ArrayList<Question>();
 
 			try {
-				connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + db + "?user=" + user + "&password=" + pass + "");
+				connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + db + "?user="
+						+ user + "&password=" + pass + "&socketTimeout=0");
 				statement = connection.createStatement();
 				resultSet = statement.executeQuery("SELECT Question_Text FROM Question;");
 				while (resultSet.next()) {
 					questions.add(new Question(resultSet.getString("Question_Text")));
 				}
+				statement.close();
 				connection.close();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			initialized = true;
 		}
 	}
