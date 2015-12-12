30e0ffad3c75abc2afbe91ddf44351fc3da855ba
11864733 <11864733@STU_BI208-03.NWTC.EDU>
2015-12-11 18:50:33 -0600

    Added labels to QuestionSubmit

--- src/trivia/gui/QuestionSubmit.java
+++ src/trivia/gui/QuestionSubmit.java
@@ -2,6 +2,7 @@ package trivia.gui;
 
 import java.awt.GridBagLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import trivia.Game;
@@ -11,8 +12,9 @@ import java.awt.Insets;
 public class QuestionSubmit extends GamePanel {
 	private JTextField answerField;
 	private JTextField questionField;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JLabel answerLabel;
+	private JLabel questionLabel;
+	
 	
 	public QuestionSubmit() {
 		createGui();
@@ -20,37 +22,64 @@ public class QuestionSubmit extends GamePanel {
 	
 	public QuestionSubmit(GameFrame gameFrame){
 		super(gameFrame);
-		Game game = gameFrame.getGame();
 		createGui();
+		gameFrame.setVisible(true);
 	}
 
 	@Override
 	protected void createGui() {
 		gameFrame.setTitle("Question Submit");
 		
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0};
-		setLayout(gbl_contentPane);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWeights = new double[] { 1.0, 0.0, 1.0 };
+		setLayout(gridBagLayout);
+		
+		questionLabel = new JLabel();
+		questionLabel.setText("Enter Question");
+		GridBagConstraints gbc_questionLabel = new GridBagConstraints();
+		gbc_questionLabel.fill = GridBagConstraints.BOTH;
+		gbc_questionLabel.gridheight = 1;
+		gbc_questionLabel.gridwidth = 1;
+		gbc_questionLabel.gridx = 0;
+		gbc_questionLabel.gridy = 0;
+		gbc_questionLabel.weightx = 0;
+		gbc_questionLabel.weighty = 0;
+		add(questionLabel, gbc_questionLabel);
+		
 		
 		questionField = new JTextField();
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.gridwidth = 4;
-		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 0;
-		gbc_textField_1.gridy = 1;
-		add(questionField, gbc_textField_1);
-		questionField.setColumns(10);
+		GridBagConstraints gbc_questionField = new GridBagConstraints();
+		gbc_questionField.fill = GridBagConstraints.BOTH;
+		gbc_questionField.gridheight = 1;
+		gbc_questionField.gridwidth = 1;
+		gbc_questionField.gridx = 0;
+		gbc_questionField.gridy = 1;
+		gbc_questionField.weightx = 0;
+		gbc_questionField.weighty = 0;
+		add(questionField, gbc_questionField);
+		
+		answerLabel = new JLabel();
+		answerLabel.setText("Enter Answer");
+		GridBagConstraints gbc_answerLabel = new GridBagConstraints();
+		gbc_answerLabel.fill = GridBagConstraints.BOTH;
+		gbc_answerLabel.gridheight = 1;
+		gbc_answerLabel.gridwidth = 1;
+		gbc_answerLabel.gridx = 0;
+		gbc_answerLabel.gridy = 2;
+		gbc_questionLabel.weightx = 0;
+		gbc_questionLabel.weighty = 0;
+		add(answerLabel, gbc_answerLabel);		
 		
 		answerField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridwidth = 4;
-		gbc_textField.gridx = 0;
-		gbc_textField.gridy = 3;
-		add(answerField, gbc_textField);
-		answerField.setColumns(10);
+		GridBagConstraints gbc_answerField = new GridBagConstraints();
+		gbc_answerField.fill = GridBagConstraints.BOTH;
+		gbc_answerField.gridheight = 1;
+		gbc_answerField.gridwidth = 1;
+		gbc_answerField.gridx = 0;
+		gbc_answerField.gridy = 3;
+		gbc_questionField.weightx = 0;
+		gbc_questionField.weighty = 0;
+		add(answerField, gbc_answerField);
 		
 	}
 }
