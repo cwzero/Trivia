fdd7fa93d278fea99f4b7893ad91f01da5140871
Ruth <Ruth@Molly>
2015-12-18 15:06:06 -0600

    Added back and submit buttons to QuestionSubmit Panel

--- src/trivia/Trivia.java
+++ src/trivia/Trivia.java
@@ -1,4 +1,4 @@
-package trivia;
+ package trivia;
 
 import java.awt.EventQueue;
 
--- src/trivia/gui/QuestionSubmit.java
+++ src/trivia/gui/QuestionSubmit.java
@@ -2,13 +2,15 @@ package trivia.gui;
 
 import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-import trivia.Game;
 import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+@SuppressWarnings("serial")
 public class QuestionSubmit extends GamePanel {
 	private JTextField answerField;
 	private JTextField questionField;
@@ -81,5 +83,56 @@ public class QuestionSubmit extends GamePanel {
 		gbc_questionField.weighty = 0;
 		add(answerField, gbc_answerField);
 		
+		GridBagConstraints gbc_backButton = new GridBagConstraints();
+		gbc_backButton.gridx = 0;
+		gbc_backButton.gridy = 4;
+		gbc_backButton.gridwidth = 1;
+		gbc_backButton.gridheight = 1;
+		gbc_backButton.fill = GridBagConstraints.BOTH;
+		gbc_backButton.weightx = 0;
+		gbc_backButton.weighty = 0;
+
+		JButton backButton = new JButton("Back");
+		backButton.setFont(southPark);
+		backButton.setOpaque(false);
+		backButton.setContentAreaFilled(false);
+		backButton.setBorderPainted(false);
+		backButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				QuestionSubmit.this.backButton_Click();
+			}
+		});
+		add(backButton, gbc_backButton);
+		
+		GridBagConstraints gbc_submitButton = new GridBagConstraints();
+		gbc_submitButton.gridx = 1;
+		gbc_submitButton.gridy = 4;
+		gbc_submitButton.gridwidth = 1;
+		gbc_submitButton.gridheight = 1;
+		gbc_submitButton.fill = GridBagConstraints.BOTH;
+		gbc_submitButton.weightx = 0;
+		gbc_submitButton.weighty = 0;
+
+		JButton submitButton = new JButton("Submit");
+		submitButton.setFont(southPark);
+		submitButton.setOpaque(false);
+		submitButton.setContentAreaFilled(false);
+		submitButton.setBorderPainted(false);
+		submitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				QuestionSubmit.this.submitButton_Click();
+			}
+		});
+		add(submitButton, gbc_submitButton);
+	}
+	
+	public void backButton_Click() {
+		new MainMenuPanel(gameFrame);
+	}
+	
+	public void submitButton_Click(){
+		//insert entries to database
 	}
 }
